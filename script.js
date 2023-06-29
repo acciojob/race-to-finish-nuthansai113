921b0db60820e2c7d55adb0604c268fa2efd835a
nuthansai113 <130201140+nuthansai113@users.noreply.github.com>
2023-06-29 10:51:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-window.promises = [];
+// Generate a random time between 1 and 5 seconds
+function getRandomTime() {
+  return Math.floor(Math.random() * 5000) + 1000;
+}
 
-// Do not change the code above this
-// add your promises to the array `promises`
+// Create an array of five promises
+const promises = Array.from({ length: 5 }, (_, index) =>
+  new Promise((resolve) => {
+    const time = getRandomTime();
+    setTimeout(() => {
+      resolve(`Promise ${index + 1} resolved after ${time / 1000} seconds`);
+    }, time);
+  })
+);
+
+// Wait for the first promise to resolve using Promise.any()
+Promise.any(promises)
+  .then((result) => {
+    // Print the result to the output div
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerText = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
