f62fc6e890b84d61748e6c3cd8343aaf3f45dfad
nuthansai113 <130201140+nuthansai113@users.noreply.github.com>
2023-07-03 22:17:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 // Generate a random time between 1 and 5 seconds
-function getRandomTime() {
-  return Math.floor(Math.random() * 5000) + 1000;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  function getRandomTime() {
+    return Math.floor(Math.random() * 5000) + 1000;
+  }
 
-// Create an array of five promises
-const promises = Array.from({ length: 5 }, (_, index) =>
-  new Promise((resolve) => {
-    const time = getRandomTime();
-    setTimeout(() => {
-      resolve(`Promise ${index + 1} resolved after ${time / 1000} seconds`);
-    }, time);
-  })
-);
+  // Create an array of five promises
+  const promises = Array.from({ length: 5 }, (_, index) =>
+    new Promise((resolve) => {
+      const time = getRandomTime();
+      setTimeout(() => {
+        resolve(`Promise ${index + 1} resolved after ${time / 1000} seconds`);
+      }, time);
+    })
+  );
 
-// Wait for the first promise to resolve using Promise.any()
-Promise.any(promises)
-  .then((result) => {
-    // Print the result to the output div
-    const outputDiv = document.getElementById('output');
-    outputDiv.innerText = result;
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+  // Wait for the first promise to resolve using Promise.any()
+  Promise.any(promises)
+    .then((result) => {
+      // Print the result to the output div
+      const outputDiv = document.getElementById("output");
+      outputDiv.innerText = result;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
